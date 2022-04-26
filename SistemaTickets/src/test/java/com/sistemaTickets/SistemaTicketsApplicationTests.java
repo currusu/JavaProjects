45ac9683221d2f09922e6ee654391f9a037031f3
currusu <natachagreco1@hotmail.com>
2022-04-25 21:17:42 -0300

    UML Commit

---
+++ SistemaTickets/src/test/java/com/sistemaTickets/SistemaTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
